63c689de63cc84fb7d57b033a45450d53f172286
Carlos Pirelli (mac) <mac@Mac-mini-de-Carlos.local>
2025-12-07 13:16:27 -0300

    Fix: Limpiar debug_variant_alert.js de código erróneo
    - Eliminar código de patch que causaba error de template
    - Dejar solo código de debug comentado
    - El template XML es el que debe manejar la visualización

--- static/src/js/debug_variant_alert.js
+++ static/src/js/debug_variant_alert.js
@@ -2,44 +2,26 @@
 
 import { patch } from "@web/core/utils/patch";
 import { ProductCard } from "@point_of_sale/app/generic_components/product_card/product_card";
-import { usePos } from "@point_of_sale/app/store/pos_hook";
-import { Component, useState, onWillStart } from "@odoo/owl";
 
-console.log("🔥 [low_stocks_product_alert_variant] Componente de alertas cargado");
+console.log("🔥 [low_stocks_product_alert_variant] debug_variant_alert.js cargado");
 
-// Patch para agregar badge de alerta de stock bajo
+// Debug deshabilitado - descomentar para debug
+/*
 patch(ProductCard.prototype, {
     setup() {
         super.setup(...arguments);
-        this.pos = usePos();
-    },
-    
-    get hasLowStockAlert() {
         const product = this.props.product;
-        return product.alert_tag && product.alert_tag !== false;
-    },
-    
-    get alertTagValue() {
-        const product = this.props.product;
-        return product.alert_tag || '';
+        
+        // Debug: Mostrar información del producto solo si tiene alert_tag
+        if (product.alert_tag) {
+            console.log("🐛 [Alert Debug] Producto CON ALERTA:", {
+                id: product.id,
+                name: product.display_name || product.name,
+                alert_tag: product.alert_tag,
+                qty_available: product.qty_available,
+                is_storable: product.is_storable || product.type === 'product',
+            });
+        }
     }
 });
-
-// Agregar template dinámicamente
-patch(ProductCard, {
-    template: `
-        <t t-inherit="point_of_sale.ProductCard" t-inherit-mode="extension" owl="1">
-            <xpath expr="//div[hasclass('product-img')]" position="inside">
-                <t t-if="hasLowStockAlert">
-                    <span class="alert_tag position-absolute top-0 start-0 translate-middle"
-                          style="background-color: #a1dafc; margin-left: 20%; margin-top: 9%; padding: 1px 10px; border-radius: 5px;">
-                        <i class="fa fa-warning text-danger" style="padding-right: 5px;"/>
-                        <t t-esc="alertTagValue"/>
-                    </span>
-                </t>
-            </xpath>
-        </t>
-    `
-});
-
-console.log("✅ [low_stocks_product_alert_variant] Patch de ProductCard aplicado");
+*/
